bfc4e446d25fe194ef26ab61eaf8877610a190ab
dchioffi <danchioffi@gmail.com>
2023-07-24 23:30:07 -0400

    JavaScript and React.js Assignment 3

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import { FaHome, FaSearch, FaBell, FaEnvelope, FaBookmark, FaListAlt, FaUser, FaEllipsisH } from "react-icons/fa";
+
 const NavigationSidebar = () => {
  const { pathname } = useLocation();
  const [ignore, tuiter, active] = pathname.split("/");
- const links = ["home",     "explore",   "notifications", "messages", "bookmarks", "lists", "profile",  "more"];
+ const links = [
+   { name: "home", icon: <FaHome /> }, 
+   { name: "explore", icon: <FaSearch /> },
+   { name: "notifications", icon: <FaBell /> },
+   { name: "messages", icon: <FaEnvelope /> },
+   { name: "bookmarks", icon: <FaBookmark /> },
+   { name: "lists", icon: <FaListAlt /> },
+   { name: "profile", icon: <FaUser /> },
+   { name: "more", icon: <FaEllipsisH /> }
+ ];
  return (
    <div className="list-group">
      {links.map((link) => 
-         <Link to={`/tuiter/${link}`} className={`list-group-item text-capitalize ${active === link ? "active" : ""}`}>
-           {link}
+         <Link to={`/tuiter/${link.name}`} className={`list-group-item text-capitalize ${active === link.name ? "active" : ""}`}>
+           {link.icon} {link.name}
          </Link>
      )}
    </div>
